dbf57f9b005d4c3e36b5f3f17f2925cfb4248549
Max Royer <mextie0@gmail.com>
2025-09-28 02:02:03 -0400

    finished!

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-user_input = input("Enter 6-string guitar tuning, low to high: ")
-default_tuning = ['E', 'A', 'D', 'G', 'B', 'E']
-SCALE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'E#', 'F', 'F#', 'G', 'G#']
+user_input = input("Enter 6-string guitar tuning, low to high, seperated by spaces: ")
+SCALE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
 user_tuning = user_input.split(' ')
 
-print(user_tuning)
-
-# convert all the notes in the user tuning to their indexes in the SCALE const
 for user_note in user_tuning:
     for note in SCALE:
         if user_note == note:
             user_tuning[user_tuning.index(user_note)] = SCALE.index(note)
 
+user_tuning = [int(n) for n in user_tuning]
 
+steps = input("Enter number of steps to transpose up or down (negative or positive): ")
+steps = int(steps)
 
+for i in range(len(user_tuning)):
+    user_tuning[i] = user_tuning[i] + steps
+    if user_tuning[i] > 11:
+        user_tuning[i] = user_tuning[i] - 11
+    if user_tuning[i] < 0:
+        user_tuning[i] = user_tuning[i] + 12
+
+for user_note in user_tuning:
+    for note in SCALE:
+        if user_note == SCALE.index(note):
+            user_tuning[user_tuning.index(user_note)] = note
+
+print("New tuning: " + user_tuning[0] + " " + user_tuning[1] + " " + user_tuning[2] + " " + user_tuning[3] + " " + user_tuning[4] + " " + user_tuning[5])
